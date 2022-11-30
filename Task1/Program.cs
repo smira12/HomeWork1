9d62b82b661dce19e125e9b6c9ad00b3ad2b9dc0
smira12 <m1ronsa89@gmail.com>
2022-12-01 01:30:50 +0300

    Add second Task

---
+++ Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Задача 2
+Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3
+*/
+
+
+Console.WriteLine("Введите первое число");
+string numberSecondStr = Console.ReadLine()??"";
+
+Console.WriteLine("Введите второе число");
+string numberFirstStr = Console.ReadLine()??"";
+
+int numberSecond = Convert.ToInt32(numberSecondStr);
+int numberFirst = Convert.ToInt32(numberFirstStr);
+
+if(numberSecond > numberFirst){
+    Console.WriteLine($"наибольшее число {numberSecond}");
+}else{
+    Console.WriteLine($"наибольшее число {numberFirst}");
+}
